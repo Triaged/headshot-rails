906d0a947118595c4c880029cbce83334f3dab45
Charlie White <charlie@triaged.co>
2014-05-22 16:54:50 -0700

    removed employee_info id

--- app/serializers/employee_info_serializer.rb
+++ app/serializers/employee_info_serializer.rb
@@ -1,5 +1,5 @@
 class EmployeeInfoSerializer < ActiveModel::Serializer
-  attributes :id, :job_title, :job_start_date, :birth_date, :cell_phone, :office_phone, :home_office_location, :current_office_location
+  attributes :job_title, :job_start_date, :birth_date, :cell_phone, :office_phone, :home_office_location, :current_office_location
 
  
 end
