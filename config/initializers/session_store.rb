862fd514d9bd7b2299f7cdf0bbd0a53d01a349fa
Charlie White <charlie@triaged.co>
2014-05-22 11:53:23 -0700

    session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-HeadshotRails::Application.config.session_store :cookie_store, key: '_headshot-rails_session'
+HeadshotRails::Application.config.session_store :cookie_store, key: '_headshot-rails_session', :domain => :all
+
+
