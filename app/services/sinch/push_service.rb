aee6431c9980bbe2d45e5e655dd8c9758c3ef1f4
Charlie White <charlie@triaged.co>
2014-06-03 10:45:32 -0700

    push notifications

--- app/services/sinch/push_service.rb
+++ app/services/sinch/push_service.rb
@@ -1,13 +1,25 @@
 class Sinch::PushService
 
-	def init (params)
-		@push_token = params[:push_token]
-		@payload = params[:payload]
+	def init (push_token, payload)
+		@push_token = push_token
+		@payload = payload
 	end
 
 	def deliver!
 		Rails.logger.info @push_token
 		Rails.logger.info @payload
+
+		notification = Grocer::Notification.new(
+			  device_token:      @push_token,
+			  alert:             'incoming message',
+			  sound: 						 'default',
+			  badge:             1,
+			  expiry:            Time.now + 60*60*12,     # optional; 0 is default, meaning the message is not stored
+			  content_available: true,                  # optional; any truthy value will set 'content-available' to 1
+				custom: {
+					"SIN" => @payload
+				}
+			)
 	end
 
 end
