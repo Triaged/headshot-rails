4a0913e1485f5a5e340325e048e20b4fbba553e8
Charlie White <charlie@triaged.co>
2014-06-11 16:00:18 -0400

    removing circular dependancy

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,8 +2,7 @@ class UserSerializer < ApplicationSerializer
   attributes :id, :first_name, :last_name, :name, :avatar_face_url, :avatar_url, :email
   
   has_one :employee_info
-  has_one :manager, include: false
-  has_many :subordinates, include: false
+  
 
   def avatar_url
   	object.avatar.url
