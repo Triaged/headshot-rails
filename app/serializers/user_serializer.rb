953dd59e2949a0e0e308b5bb92702818e803ad6c
Charlie White <charlie@triaged.co>
2014-06-11 15:22:36 -0400

    adding reports to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,8 @@ class UserSerializer < ApplicationSerializer
   attributes :id, :first_name, :last_name, :name, :avatar_face_url, :avatar_url, :email
   
   has_one :employee_info
+  has_one :manager
+  has_many :subordinates
 
   def avatar_url
   	object.avatar.url
