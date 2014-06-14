885f7fdf40216905af55f37c5dbec38a37c16628
Charlie White <charlie@triaged.co>
2014-06-14 11:00:51 -0400

    admin company index

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -1,2 +1,12 @@
 class Admin::CompaniesController < AdminController
+
+	def index
+    @companies = Company.all
+  end
+
+  # GET /admin/providers/1
+  def show
+  	@company = Company.find(params[:id])
+  end
+
 end
