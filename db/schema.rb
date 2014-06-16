256e830c491fc34015bb2f637ae7ec3735af36a8
JT White <jt@triaged.co>
2014-06-16 18:35:10 -0400

    added new stylesheets - typography, buttons, colors

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616174751) do
-
+ActiveRecord::Schema.define(version: 20140616210739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
