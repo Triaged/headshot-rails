cc01a4e8ac58539afcf0e735933dc0c148c37bfc
JT White <jt@triaged.co>
2014-06-16 11:45:55 -0400

    Navigation links - Signout removed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140613142706) do
+ActiveRecord::Schema.define(version: 20140613143000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20140613142706) do
     t.integer  "company_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "users_count", default: 0
   end
 
   create_table "employee_infos", force: true do |t|
