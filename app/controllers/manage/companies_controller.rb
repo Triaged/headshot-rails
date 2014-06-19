0cb0e2c422361746758f5cd0650a0e25e67ec5ba
Charlie White <charlie@triaged.co>
2014-06-19 12:21:38 -0400

    company page

--- app/controllers/manage/companies_controller.rb
+++ app/controllers/manage/companies_controller.rb
@@ -1,7 +1,25 @@
 class Manage::CompaniesController < ApplicationController
+	before_action :set_company
 
 	def show
-    @company = current_company
   end
+
+  def update
+  	if @company.update(company_params)
+      redirect_to manage_company_path(), notice: 'Company was successfully updated.'
+    else
+      render :show
+    end
+  end
+
+private
+	
+	def set_company
+		@company = current_company
+	end
+
+	def company_params
+		params[:company].permit(:name, :uses_departments)
+	end
 
 end
