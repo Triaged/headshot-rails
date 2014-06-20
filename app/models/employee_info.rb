7a35fc30f8f0384768a0a3202cbb9cbfb5f60d70
Charlie White <charlie@triaged.co>
2014-06-20 11:43:35 -0400

    phony_rails

--- app/models/employee_info.rb
+++ app/models/employee_info.rb
@@ -1,5 +1,7 @@
 class EmployeeInfo < ActiveRecord::Base
 
 	belongs_to :user
-	
+
+	phony_normalize :cell_phone, :default_country_code => 'US'
+	phony_normalize :office_phone, :default_country_code => 'US'
 end
