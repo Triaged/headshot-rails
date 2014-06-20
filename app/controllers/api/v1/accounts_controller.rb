3bc9d6b69d962e4ee829e86af781781796cec34c
Charlie White <charlie@triaged.co>
2014-06-20 11:06:33 -0400

    typos

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -12,9 +12,8 @@ class API::V1::AccountsController < APIController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params[:user].permit(:first_name, :last_name, :avatar, :department_id, :manager_id, :primary_office_location_id
-    	employee_info: [:job_title, :cell_phone, :office_phone, :job_start_date, :birth_date],
-    	device: [:service, :token, :os_version]
+    params[:user].permit(:first_name, :last_name, :avatar, :department_id, :manager_id, :primary_office_location_id,
+    	employee_info_attributes: [:job_title, :cell_phone, :office_phone, :job_start_date, :birth_date]
     )
   end
 
