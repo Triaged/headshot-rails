f58a20a14e63991b3a7d897aa54c49e0eca9fba7
Charlie White <charlie@triaged.co>
2014-06-20 10:52:17 -0400

    primary office id

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -12,9 +12,10 @@ class API::V1::AccountsController < APIController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params[:user].permit(:first_name, :last_name, :avatar, :department_id, :manager_id, :office_location_id
+    params[:user].permit(:first_name, :last_name, :avatar, :department_id, :manager_id, :primary_office_location_id
     	employee_info: [:job_title, :cell_phone, :office_phone, :job_start_date, :birth_date],
-    	device: [:service, :token])
+    	device: [:service, :token, :os_version]
+    )
   end
 
 private
