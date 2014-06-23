910ca3c6d2cc460629cb8979e603cd8afe1d0e2e
Charlie White <charlie@triaged.co>
2014-06-23 16:37:28 -0400

    json response

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -18,7 +18,7 @@ class API::V1::AccountsController < APIController
   def update_password
     if @user.update_with_password(password_params)
       # Sign in the user by passing validation in case his password changed
-      render :json => 'ok', :status => 200
+      render :json => { "message" : "ok" }, :status => 200
     else
     	render :json=> "Error with updating your password", :status=>401
     end
