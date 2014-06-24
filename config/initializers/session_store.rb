b81052eb7dcc08629e9f5ac61c0a46b117b349ee
Charlie White <charlie@triaged.co>
2014-06-24 17:11:11 -0400

    html titles

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-HeadshotRails::Application.config.session_store :active_record_store, key: '_headshot-rails_session', domain: :all, tld_length: 2
+HeadshotRails::Application.config.session_store :active_record_store, key: '_badge-rails_session', domain: :all, tld_length: 2
