0f8e7446f6d3f00da442991eecebbfe58707fd3b
Charlie White <charlie@triaged.co>
2014-06-24 10:57:58 -0400

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,7 +50,7 @@ private # ----------------------------------------------------
 	# based on the subdomain.  You can change this to whatever best fits your
 	# application.
 	def validate_subdomain
-	    redirect_to '/404.html' if (company_subdomain? && (current_company.nil? || current_user.company != current_company))
+	    redirect_to root_url(subdomain: nil) if (company_subdomain? && (current_company.nil? || !current_user || current_user.company != current_company))
 	end
 
 end
