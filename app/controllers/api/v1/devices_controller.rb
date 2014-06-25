321c54aaab98dcd1790c25ff9750aa902e1e5fea
Charlie White <charlie@triaged.co>
2014-06-25 12:19:09 -0400

    device logic

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -1,12 +1,14 @@
 class API::V1::DevicesController < APIController
 
 	def create
-		if device_params[:token]
+		# First look for a device with the application_id
+		@device = current_user.devices.where(application_id: device_params[:application_id], service: device_params[:service]).first
+
+		unless @device
+			# then look for a device with the device token, or a create a new one
 			@device = current_user.devices.find_or_initialize_by(token: device_params[:token], service: device_params[:service])
-		else
-			@device = current_user.devices.find_or_initialize_by(application_id: device_params[:application_id], service: device_params[:service])
-		end	
-		
+		end
+
 		@device.update(device_params)
 		render :json => { "message" => "ok" }, :status => 200
 	end
