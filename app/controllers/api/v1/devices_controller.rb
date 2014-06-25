85ca97a9fe0df707db8c1c70ca17e116df2645b0
Charlie White <charlie@triaged.co>
2014-06-25 11:09:06 -0400

    device endpoint logic

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -1,7 +1,12 @@
 class API::V1::DevicesController < ApplicationController
 
 	def create
-		@device = current_user.devices.find_or_initialize_by(token: device_params[:token], service: device_params[:service])
+		if device_params[:token]
+			@device = current_user.devices.find_or_initialize_by(token: device_params[:token], service: device_params[:service])
+		else
+			@device = current_user.devices.find_or_initialize_by(application_id: device_params[:application_id], service: device_params[:service])
+		end	
+		
 		@device.update(device_params)
 		respond_with @device, location: api_v1_device_path(@device)
 	end
@@ -9,6 +14,7 @@ class API::V1::DevicesController < ApplicationController
 private 
 	
 	def device_params
-		params[:device].permit(:token, :os_version, :service)
+		params[:device].permit(:token, :os_version, :service, :application_id)
 	end
+
 end
