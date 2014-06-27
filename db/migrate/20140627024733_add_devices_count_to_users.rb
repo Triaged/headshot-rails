e171f976d7ea4bde629404ae883e0d64b9eeb8ad
Charlie White <charlie@triaged.co>
2014-06-26 22:54:05 -0400

    default devices_count to 0

--- db/migrate/20140627024733_add_devices_count_to_users.rb
+++ db/migrate/20140627024733_add_devices_count_to_users.rb
@@ -1,5 +1,5 @@
 class AddDevicesCountToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :devices_count, :integer
+    add_column :users, :devices_count, :integer, default: 0
   end
 end
