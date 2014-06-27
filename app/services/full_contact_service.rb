a0622a769d993756058cc3259eaa337981ac321e
Charlie White <charlie@triaged.co>
2014-06-27 17:39:41 -0400

    save user avatar from full contact

--- app/services/full_contact_service.rb
+++ app/services/full_contact_service.rb
@@ -22,6 +22,7 @@ class FullContactService
 		# Do a first pass, looking for linkedIn, Gravatar, or Angelist
 		photos.each do |photo|
 			if photo["type"] == "linkedin" || photo["type"] == "angellist" || photo["type"] == "gravatar"
+				puts "updating users avatar"
 				@user.update(remote_avatar_url: photos.first["url"])
 				return
 			end
