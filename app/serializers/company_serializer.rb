13f8a2172120419461c5617ac4f18b58b60ca682
Charlie White <charlie@triaged.co>
2014-07-09 13:17:20 -0400

    updated_at params

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -4,4 +4,16 @@ class CompanySerializer < ApplicationSerializer
   has_many :users
   has_many :office_locations
   has_many :departments
+
+  def users
+  	@options[:updated_at] ? object.users.where(updated_at: @options[:updated_at]) : object.users
+  end
+
+  def office_locations
+  	@options[:updated_at] ? object.office_locations.where(updated_at: @options[:updated_at]) : object.users
+  end
+
+  def departments
+  	@options[:updated_at] ? object.departments.where(updated_at: @options[:updated_at]) : object.users
+  end
 end
