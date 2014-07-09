7e9c8286685170009e10c196844a6254525dcd23
Charlie White <charlie@triaged.co>
2014-07-09 13:50:54 -0400

    greater than

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -6,14 +6,14 @@ class CompanySerializer < ApplicationSerializer
   has_many :departments
 
   def users
-  	@options[:updated_at] ? object.users.where(updated_at: DateTime.parse(@options[:updated_at])) : object.users
+  	@options[:updated_at] ? object.users.where("updated_at >= ?", DateTime.parse(@options[:updated_at])) : object.users
   end
 
   def office_locations
-  	@options[:updated_at] ? object.office_locations.where(updated_at: DateTime.parse(@options[:updated_at])) : object.users
+  	@options[:updated_at] ? object.office_locations.where("updated_at >= ?", DateTime.parse(@options[:updated_at])) : object.users
   end
 
   def departments
-  	@options[:updated_at] ? object.departments.where(updated_at: DateTime.parse(@options[:updated_at])) : object.users
+  	@options[:updated_at] ? object.departments.where("updated_at >= ?", DateTime.parse(@options[:updated_at])) : object.users
   end
 end
