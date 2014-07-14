1cb138c5e6cbbb599f92b302c95aa0ed0d455433
Charlie White <charlie@triaged.co>
2014-07-14 16:36:01 -0400

    protect against not finding a device

--- app/services/sinch/push_service.rb
+++ app/services/sinch/push_service.rb
@@ -7,13 +7,18 @@ class Sinch::PushService
 		@payload = params[:payload]
 		@body = params[:message_body]
 		@author = params[:user]
+		@can_deliver = true
+	rescue
+		@can_deliver = false
 	end
 
 	def deliver!
-		PushService.new(@user.id).deliver(
-			"#{@author.first_name.capitalize}: #{@body}".truncate(150),
-			increase_badge_count=true,
-			custom_payload = { "SIN" => @payload }
-		)
+		if @can_deliver
+			PushService.new(@user.id).deliver(
+				"#{@author.first_name.capitalize}: #{@body}".truncate(150),
+				increase_badge_count=true,
+				custom_payload = { "SIN" => @payload }
+			)
+		end
 	end
 end
