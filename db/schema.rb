ef6029d8d2643c8ab69b0d8edf2c0cfca0648515
JT White <jt@triaged.co>
2014-07-18 14:40:48 -0400

    Landing Page - hero tagline typography fix

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140702185044) do
 
   create_table "departments", force: true do |t|
     t.string   "name"
+    t.boolean  "shared"
     t.integer  "company_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -188,36 +189,40 @@ ActiveRecord::Schema.define(version: 20140702185044) do
   add_index "teams", ["company_id"], name: "index_teams_on_company_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                      default: "", null: false
-    t.string   "encrypted_password",         default: "", null: false
+    t.string   "email",                      default: "",    null: false
+    t.string   "encrypted_password",         default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",              default: 0,  null: false
+    t.integer  "sign_in_count",              default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.datetime "deleted_at"
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "company_id"
+    t.string   "title"
     t.integer  "team_id"
+    t.string   "phone_number"
     t.string   "avatar"
     t.integer  "manager_id"
+    t.date     "start_date"
+    t.date     "birthday"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "authentication_token"
     t.integer  "department_id"
     t.boolean  "admin"
+    t.datetime "deleted_at"
     t.integer  "primary_office_location_id"
     t.integer  "current_office_location_id"
     t.integer  "devices_count",              default: 0
     t.string   "slug"
-    t.boolean  "sharing_office_location"
+    t.boolean  "sharing_office_location",    default: false
   end
 
   add_index "users", ["company_id"], name: "index_users_on_company_id", using: :btree
