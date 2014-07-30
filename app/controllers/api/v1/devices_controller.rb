d82a1ecfb0d4980e9f0e7f4d98da71538db7f874
Charlie White <charlie@triaged.co>
2014-07-30 13:48:32 -0400

    logging

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -4,7 +4,7 @@ class API::V1::DevicesController < APIController
 		@device = current_user.devices.where(application_id: device_params[:application_id], service: device_params[:service]).first ||
 									current_user.devices.where(token: device_params[:token], service: device_params[:service]).first
 
-		Rails.logger.info @device.inspect
+		puts @device.inspect
 		unless @device
 			@device = current_user.devices.find_or_initialize_by(token: device_params[:token], service: device_params[:service])
 		end
