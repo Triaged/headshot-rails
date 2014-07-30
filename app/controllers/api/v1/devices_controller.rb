47abe211f3dcce53080009482fffac406bda365c
Charlie White <charlie@triaged.co>
2014-07-30 13:42:45 -0400

    logging

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -1,12 +1,11 @@
 class API::V1::DevicesController < APIController
 
 	def create
-		# First look for a device with the application_id
 		@device = current_user.devices.where(application_id: device_params[:application_id], service: device_params[:service]).first ||
 									current_user.devices.where(token: device_params[:token], service: device_params[:service]).first
 
+		Rails.logger.info @device.inspect
 		unless @device
-			# then look for a device with the device token, or a create a new one
 			@device = current_user.devices.find_or_initialize_by(token: device_params[:token], service: device_params[:service])
 		end
 
