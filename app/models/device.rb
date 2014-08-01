116840973ebdbb2519b08fccbda1d45873852725
Charlie White <charlie@triaged.co>
2014-08-01 13:13:17 -0400

    GCM registration

--- app/models/device.rb
+++ app/models/device.rb
@@ -15,10 +15,19 @@ class Device < ActiveRecord::Base
   def create_sns_endpoint
   	return unless self.token_changed?
   	client = AWS::SNS.new.client
-		response = client.create_platform_endpoint(
-		  platform_application_arn: "arn:aws:sns:us-west-2:297824293218:app/APNS/badge-staging",
-		  token: self.token
-		)
+
+  	if self.service == "ios"
+	  	response = client.create_platform_endpoint(
+			  platform_application_arn: "arn:aws:sns:us-west-2:297824293218:app/APNS/badge-staging",
+			  token: self.token
+			)
+		else
+			response = client.create_platform_endpoint(
+			  platform_application_arn: "arn:aws:sns:us-west-2:297824293218:app/GCM/badge-staging-cgm",
+			  token: self.token
+			)
+		end
+
 		self.arn = response[:endpoint_arn]
   end
 
