eb96e628b7af0e44616afa234dff0663a52377c2
Charlie White <charlie@triaged.co>
2014-08-02 16:48:03 -0400

    typo on email

--- app/services/push_service.rb
+++ app/services/push_service.rb
@@ -61,7 +61,7 @@ class PushService
 		client = AWS::SNS::Client.new
 		apns_payload = { 
 			"aps" => { 
-				"alert" => alert.truncate(100), 
+				"alert" => alert.truncate(80), 
 				"badge" => (increase_badge_count ? device.count : 0),
 				"content_available" => true,
 				"expiry" => Time.now + 60*60*12,
