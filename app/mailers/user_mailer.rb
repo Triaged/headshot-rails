81ee6aab12d6b4b215c1492c8d15fd21dfeec9dc
Charlie White <charlie@triaged.co>
2014-08-02 18:01:32 -0400

    admin welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,17 @@ class UserMailer < Devise::Mailer
   def confirmation_instructions(record, token, opts={})
   	mail = super
     # your custom logic
-    admin = record.company.admin_user
-		mail.subject = "#{admin.first_name.capitalize} invited you to Badge!"
-		mail.from = "\"#{admin.full_name} via Badge\" <team@badge.co>"
+    show_admin = record.admin ? true : false
+
+    unless show_admin
+      admin = record.company.admin_user
+  		mail.subject = "#{admin.first_name.capitalize} invited you to Badge!"
+  	else
+      mail.subject = "Welcome to Badge!"
+      mail.template = "confirmation_instructions_admin"
+    end
+
+    mail.from = "\"#{admin.full_name} via Badge\" <team@badge.co>"
 
     mail
 	end
