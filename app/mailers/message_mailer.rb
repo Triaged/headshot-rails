53887a2ace6468a3f230b60a0335468d8c4b834f
Charlie White <charlie@triaged.co>
2014-08-03 17:52:27 -0400

    segment.io

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -5,6 +5,7 @@ class MessageMailer < ActionMailer::Base
 		@recipient = User.find(recipient_id)
 		@sender = User.find(sender_id)
 		@message = message
+		@token = Devise.token_generator.digest(self, :confirmation_token, @recipient.confirmation_token)
 
 		mail(to: @recipient.email, from: "\"#{@sender.full_name} via Badge\" <team@badge.co>", 
 					subject: "#{@sender.full_name} sent you a message")
