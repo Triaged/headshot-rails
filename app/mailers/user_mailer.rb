11a8df960a06b46dd9864666dec46f5a9ecbe0b3
Charlie White <charlie@triaged.co>
2014-08-03 12:54:30 -0400

    confirmation subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,16 +9,16 @@ class UserMailer < Devise::Mailer
 
     unless show_admin
       admin = record.company.admin_user
-  		mail.subject = "#{admin.first_name.capitalize} invited you to Badge!"
+  		subject = "#{admin.first_name.capitalize} invited you to Badge!"
       mail.from = "\"#{admin.full_name} via Badge\" <team@badge.co>"
       template_name = "confirmation_instructions"
   	else
-      mail.subject = "Welcome to Badge!"
+      subject = "Welcome to Badge!"
       mail.from = "\"Badge Team\" <team@badge.co>"
       template_name = "confirmation_instructions_admin"
     end
 
-    mail do |format|
+    mail(subject: subject) do |format|
       format.html { render template_name }
     end
 
