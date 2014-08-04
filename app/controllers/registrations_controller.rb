eb470203fc0cb87b0c3f91637f2514e8ba527ad9
Charlie White <charlie@triaged.co>
2014-08-04 10:23:07 -0400

    change password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :update_sanitized_params, if: :devise_controller?
+  skip_before_filter :authenticate_user!, only: [:update]
 
   def update_sanitized_params
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:first_name, :email, :password, :password_confirmation)}
