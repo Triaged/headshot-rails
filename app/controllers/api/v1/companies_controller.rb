f2213e7f9ca32c2142140ddcdb3a516cd66ebb8d
Charlie White <charlie@triaged.co>
2014-08-04 14:51:20 -0400

    company updated since

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,7 +1,7 @@
 class API::V1::CompaniesController < APIController
 
 	def show
-		respond_with Company.where(id: current_company.id).includes(:users, :departments, :office_locations), updated_at: params[:timestamp]
+		respond_with Company.where(id: current_company.id).includes(:users, :departments, :office_locations), updated_at: params[:timestamp].to_f
 	end
 
 end
