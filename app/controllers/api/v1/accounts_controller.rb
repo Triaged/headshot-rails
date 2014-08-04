4e609497157fe8934aedbf272796b2c55a4c0612
Charlie White <charlie@triaged.co>
2014-08-04 11:41:34 -0400

    allow email changes

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -48,7 +48,7 @@ private
 
 	# Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params[:user].permit(:first_name, :last_name, :avatar, :department_id, :manager_id, :primary_office_location_id, :sharing_office_location,
+    params[:user].permit(:first_name, :last_name, :email, :avatar, :department_id, :manager_id, :primary_office_location_id, :sharing_office_location,
     	employee_info_attributes: [:job_title, :cell_phone, :office_phone, :job_start_date, :birth_date]
     )
   end
