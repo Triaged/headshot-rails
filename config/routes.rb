a0627d5b745edec8ba93891928c17d5f2906519d
Charlie White <charlie@triaged.co>
2014-08-04 10:32:19 -0400

    change password

--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,7 @@ HeadshotRails::Application.routes.draw do
       patch '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
       get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
       put 'users/:id' => 'devise/registrations#update', :as => 'user_registration' 
+      put '/users/password' => 'devise/passwords#update'
   end
   devise_for :users, :skip => [:registrations], :controllers => {omniauth_callbacks: "omniauth_callbacks",  :confirmations => "confirmations"}
   
