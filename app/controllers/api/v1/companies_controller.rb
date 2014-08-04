b8cb2b71278ebe408caddaeca5decb82a21a20af
Charlie White <charlie@triaged.co>
2014-08-04 15:37:34 -0400

    timestamp

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,7 +1,7 @@
 class API::V1::CompaniesController < APIController
 
 	def show
-		timestamp = Time.at(params[:timestamp].to_f/100).to_s
+		timestamp = Time.at(params[:timestamp].to_f/1000).to_s
 		respond_with Company.where(id: current_company.id).includes(:users, :departments, :office_locations), updated_at: timestamp
 	end
 
