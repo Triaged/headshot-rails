85a8e747e95dbffe01e8df767388f7f8530d4c2e
Charlie White <charlie@triaged.co>
2014-08-06 09:26:56 -0400

    google api failed flash msg

--- app/controllers/manage/import_controller.rb
+++ app/controllers/manage/import_controller.rb
@@ -9,7 +9,7 @@ class Manage::ImportController < ManageController
 		crendentials_id = session[:credentials_id]
 		@import = UserImport.new(current_user.id, current_company.id).import_users(@provider.id, crendentials_id)
 		unless @import.errors.empty?
-			flash[:error] = "Authentication failed. Please ensure you are a #{@provider.title} admin and try again."
+			flash[:error] = "Authentication failed. Please ensure you are a #{@provider.title} admin and that API access is enabled."
 			redirect_to manage_import_index_url
 		end
 	end
