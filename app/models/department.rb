b2b60a6ddfb172155fbc66adeab0dfdcb10f7ecf
Charlie White <charlie@triaged.co>
2014-08-06 12:10:53 -0400

    push departments

--- app/models/department.rb
+++ app/models/department.rb
@@ -4,11 +4,30 @@ class Department < ActiveRecord::Base
 
 	validates :name, uniqueness: {scope: :company}
 
+	after_create :push_entity
+
 	DEFAULT_DEPARTMENTS = ['Engineering', 'Services', 'Operations', 'Marketing', 'Human Resources', 'Finance', 'Product', 'Project Management', 
 		'Purchasing', 'Sales', 'IT', 'Design', 'Quality Assurance', 'Operations', 'Marketing', 'Client Services', 'Customer Support']
 
 	def self.create_default_departments company
 		DEFAULT_DEPARTMENTS.each {|dept| Department.create(name: dept, company: company)}
+	end
+
+	def push_entity
+		users = self.company.users
+		devices = users.collect {|user| user.devices.where(service: 'android')}.to_a.uniq{ |device| device.token }
+
+		client = AWS::SNS::Client.new
+
+		devices.each do |device|
+			gcm_payload = {data: {department: self.id}}.to_json
+			message = { "default" => alert, "GCM" => gcm_payload }.to_json
+			client.publish( message: message, target_arn: device.arn, message_structure: 'json' )
+		end
+	rescue
+		Rails.logger.info "delivery to device: #{device.inspect} failed"
+		device.delete_sns_endpoint
+		device.destroy
 	end
 
 end
