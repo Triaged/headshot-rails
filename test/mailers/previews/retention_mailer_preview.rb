2f07ceba7d5eca775c42b4ac085650f5bc4c7833
Charlie White <charlie@triaged.co>
2014-08-10 16:18:32 -0400

    user mailer folder

---
+++ test/mailers/previews/retention_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/retention_mailer
+class RetentionMailerPreview < ActionMailer::Preview
+
+end
---
+++ test/mailers/retention_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RetentionTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
