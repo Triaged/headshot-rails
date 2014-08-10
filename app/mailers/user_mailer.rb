28698fd0177378f30d149ac823b4ab76119a26ae
Charlie White <charlie@triaged.co>
2014-08-10 18:00:46 -0400

    rake task

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -24,13 +24,5 @@ class UserMailer < Devise::Mailer
 
   end
 
-  def retention_first(user_id)
-    @recipient = User.find(user_id)
-
-    @token, secure_token = Devise.token_generator.generate(User, :confirmation_token)
-    @recipient.update_attributes(confirmation_token: secure_token)
-
-    mail(to: @recipient.email, from:  "\"Badge Team\" <team@badge.co>", 
-          subject: "Your invitation to Badge is waiting.")
-  end
+  
 end
