4f5555b45d0e249c51c113affd966e12195b7e89
Charlie White <charlie@triaged.co>
2014-08-10 20:35:52 -0400

    no more exclude current user

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -2,7 +2,7 @@ class API::V1::CompaniesController < APIController
 
 	def show
 		timestamp = Time.at(params[:timestamp].to_f/1000).to_s
-		exclude_current_user = params[:exclude_current_user]
+		exclude_current_user = false #params[:exclude_current_user]
 		respond_with Company.where(id: current_company.id).includes(:users, :departments, :office_locations), updated_at: timestamp, exclude_current_user: exclude_current_user
 	end
 
