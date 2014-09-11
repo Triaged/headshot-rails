815f2917db64867536c8d64697d3ceaebd1504a1
Charlie White <charlie@triaged.co>
2014-09-11 11:23:28 -0400

    expire company call

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,6 +1,7 @@
 class API::V1::CompaniesController < APIController
 
 	def show
+		expires_now
 		timestamp = Time.at(params[:timestamp].to_f/1000).to_s
 		respond_with current_company, updated_at: timestamp
 	end
