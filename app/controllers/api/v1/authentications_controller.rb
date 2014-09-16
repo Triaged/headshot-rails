4d3736c2aa4b2d0746422c7524e686d718b4453a
Bashar Harfoush <a5e3b3@yahoo.com>
2014-09-16 11:00:29 -0400

    Skipe notification email when creating a new user

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -1,28 +1,30 @@
 class API::V1::AuthenticationsController < APIController
 
-    skip_before_filter :authenticate_user_from_token!
-    skip_before_filter :authenticate_user!
-    skip_before_filter :current_company
+	skip_before_filter :authenticate_user_from_token!
+	skip_before_filter :authenticate_user!
+	skip_before_filter :current_company
 
-    def create
-        @user = User.find_by( email: auth_params[:email] ) \
-            || User.find_by( phone_number: auth_params[:phone_number] )
+	def create
+		@user = User.find_by( email: auth_params[:email] ) \
+			|| User.find_by( phone_number: auth_params[:phone_number] )
 
-        unless @user
-            @user = User.create( auth_params )
-        end
+		unless @user
+			@user = User.new( auth_params )
+			@user.skip_confirmation_notification!
+			@user.save
+		end
 
-        AuthenticationService.new(@user).generate_and_deliver!
+		AuthenticationService.new(@user).generate_and_deliver!
 
-        render :json => { "id" => @user.id }, :status => 200
+		render :json => { "id" => @user.id }, :status => 200
 
-    end
+	end
 
-    def valid
-    end
+	def valid
+	end
 
-  def auth_params
-    params[:auth_params].permit( :first_name, :last_name, :email, :phone_number )
-  end
+	def auth_params
+		params[:auth_params].permit( :first_name, :last_name, :email, :phone_number )
+	end
 
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,28 +1,28 @@
-class UserMailer < Devise::Mailer   
-  helper :application # gives access to all helpers defined within `application_helper`.
-  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+class UserMailer < Devise::Mailer
+	helper :application # gives access to all helpers defined within `application_helper`.
+	include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
 
-  def confirmation_instructions(record, token, opts={})
-  	mail = super
-    # your custom logic
-    show_admin = record.admin ? true : false
+	def confirmation_instructions(record, token, opts={})
+		mail = super
+		# your custom logic
+		show_admin = record.admin ? true : false
 
-    unless show_admin
-      admin = record.company.admin_user
-  		subject = "#{admin.first_name.capitalize} invited you to Badge!"
-      mail.from = "\"#{admin.full_name} via Badge\" <team@badge.co>"
-      template_name = "confirmation_instructions"
-  	else
-      subject = "Welcome to Badge!"
-      mail.from = "\"Badge Team\" <team@badge.co>"
-      template_name = "confirmation_instructions_admin"
-    end
+		unless show_admin
+			admin = record.company.admin_user
+			subject = "#{admin.first_name.capitalize} invited you to Badge!"
+			mail.from = "\"#{admin.full_name} via Badge\" <team@badge.co>"
+			template_name = "confirmation_instructions"
+		else
+			subject = "Welcome to Badge!"
+			mail.from = "\"Badge Team\" <team@badge.co>"
+			template_name = "confirmation_instructions_admin"
+		end
 
-    mail(subject: subject) do |format|
-      format.html { render template_name }
-    end
+		mail(subject: subject) do |format|
+			format.html { render template_name }
+		end
 
-  end
+	end
 
-  
+
 end
