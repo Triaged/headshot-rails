ced96f1f6f5201bdc50a9098f4a4024974f9f250
Charlie White <charlie@triaged.co>
2014-10-07 10:54:59 -0400

    registrations controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	extend FriendlyId
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
-	devise :database_authenticatable, :registerable, :omniauthable#, :confirmable,
+	devise :database_authenticatable, :registerable, :omniauthable,# :confirmable,
 		:recoverable, :rememberable, :trackable, :validatable, :async
 
 
