1ecf76fd2c50e835cfe9cbc5077d2d066ed5dafa
Bashar Harfoush <a5e3b3@yahoo.com>
2014-10-27 13:18:29 -0400

    Move thread to its own resource

--- app/assets/javascripts/messaging/router.js
+++ app/assets/javascripts/messaging/router.js
@@ -2,17 +2,13 @@ Messaging.Router.reopen({
     rootURL: "/messaging/"
 });
 
-
-
 // A route is responsible for gathering the data, populating the
 // controller and showing the template.
 Messaging.Router.map(function() {
     // shows all the threads
     this.resource("threads", function() {
-        this.route("thread", { path: "/:thread_id" });
+        this.resource("thread", { path: "/:thread_id" });
     });
-    // shows a single thread
-
 });
 
 // shows all the threads
@@ -26,8 +22,14 @@ Messaging.ThreadsRoute = Ember.Route.extend({
     }
 });
 
+Messaging.ThreadsIndexRoute = Ember.Route.extend({
+    afterModel: function(threads, transition) {
+        this.transitionTo('thread', threads.get('firstObject'));
+    }
+});
+
 // shows a single thread
-Messaging.ThreadsThreadRoute = Ember.Route.extend({
+Messaging.ThreadRoute = Ember.Route.extend({
     model: function(params) {
         return this.store.find('thread', params.thread_id);
     }
